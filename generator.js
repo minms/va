c79bcf524115e38ee1a7314e19c500990d46f8b8
minms <minms@imac.local>
2020-03-03 11:02:47 +0800

    update dependencies

--- generator.js
+++ generator.js
@@ -1,22 +1,22 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const isBinary = require('isbinaryfile')
+const isBinary = require('isbinaryfile');
 
 async function generate(dir, files, base = '', rootOptions = {}) {
-  const glob = require('glob')
+  const glob = require('glob');
 
   glob.sync('**/*', {
     cwd: dir,
     nodir: true
   }).forEach(rawPath => {
-    const sourcePath = path.resolve(dir, rawPath)
-    const filename = path.join(base, rawPath)
+    const sourcePath = path.resolve(dir, rawPath);
+    const filename = path.join(base, rawPath);
 
     if (isBinary.sync(sourcePath)) {
       files[filename] = fs.readFileSync(sourcePath) // return buffer
     } else {
-      let content = fs.readFileSync(sourcePath, 'utf-8')
+      let content = fs.readFileSync(sourcePath, 'utf-8');
       if (path.basename(filename) === 'manifest.json') {
         content = content.replace('{{name}}', rootOptions.projectName || '')
       }
@@ -35,25 +35,25 @@ module.exports = (api, options, rootOptions) => {
   // 安装一些基础公共库
   api.extendPackage({
     dependencies: {
-      "va-core": "^3.6.4"
+      "va-core": "^1.0.0"
     },
     devDependencies: {
       "webpack": "^4.41.6"
     }
-  })
+  });
 
   // 公共基础目录和文件
   api.render(async function (files) {
     Object.keys(files).forEach(name => {
-      if (['vue.config.js'].includes(name)) return
+      if (['vue.config.js'].includes(name)) return;
 
       delete files[name]
-    })
-    await generate(path.resolve(__dirname, './template/default/src'), files, 'src', rootOptions)
-  })
+    });
+    await generate(path.resolve(__dirname, './template/src'), files, 'src', rootOptions)
+  });
 
   api.render({
-    './.gitignore': './template/default/_gitignore',
-    './public/index.html': './template/default/public/index.html'
+    './.gitignore': './template/_gitignore',
+    './public/index.html': './template/public/index.html'
   })
-}
+};
